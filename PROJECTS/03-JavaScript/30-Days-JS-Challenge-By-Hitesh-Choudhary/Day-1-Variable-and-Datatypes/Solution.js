ad8280a698ab1595561c054be96998daff861bdc
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2024-07-15 12:28:39 +0530

    Create Solution.js

---
+++ PROJECTS/03-JavaScript/30-Days-JS-Challenge-By-Hitesh-Choudhary/Day-1-Variable-and-Datatypes/Solution.js
@@ -0,0 +1,53 @@
+// Activity 1 : Variable Declaration
+// Task 1
+
+var num = 10;
+console.log(num);
+
+// Task 2
+
+let str = "Chaitaly";
+console.log(str);
+
+// Activity 2 : Constant Declaration
+// Task 3
+
+const bool_status = true;
+console.log(bool_status);
+
+// Activity 3 : Data types
+// Task 4
+
+let num1 = 2;
+let str1 = "Chaitaly";
+let result = false;
+let obj = {
+  name: "Chaitaly",
+  age: "24",
+};
+let arr = [1, 2, 3, 4, 5];
+
+console.log(typeof num1);
+console.log(typeof str1);
+console.log(typeof result);
+console.log(typeof obj);
+console.log(typeof arr);
+
+// Activity 4 : Reassigning variables
+// Task 5
+
+let value = 4;
+console.log(value);
+value = 6;
+console.log(value);
+
+
+// Activity 5 : Understanding `const`
+//  Task 6
+
+const val = 2;
+console.log(val);
+val = 5;
+console.log(val);
+
+// Description : It will give error as we can't reassign const variable
