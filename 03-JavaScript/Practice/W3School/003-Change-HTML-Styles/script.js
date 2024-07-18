12c033a436e75b1de3f6483da3d6113a59a1cce9
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2024-07-18 18:24:07 +0530

    Create script.js

---
+++ 03-JavaScript/Practice/W3School/003-Change-HTML-Styles/script.js
@@ -0,0 +1,20 @@
+function changeColorToBlue(){
+    const res = document.getElementById('demo');
+    res.style.color ="blue";
+}
+
+function changeFontSize(){
+    const res = document.getElementById('demo');
+    res.style.fontSize ="30px";
+}
+
+function changeBackgroundColor(){
+    const res = document.getElementById('demo');
+    res.style.backgroundColor ="yellow";
+}
+
+function changeFontFamily(){
+    const res = document.getElementById('demo');
+    res.style.fontFamily ="cursive";
+}
+
