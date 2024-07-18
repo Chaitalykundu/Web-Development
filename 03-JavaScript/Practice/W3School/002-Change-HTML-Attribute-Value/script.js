a82039e71a76437bf27ceeab94ce5015e68916e9
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2024-07-18 18:23:24 +0530

    Create script.js

---
+++ 03-JavaScript/Practice/W3School/002-Change-HTML-Attribute-Value/script.js
@@ -0,0 +1,9 @@
+function changeHtmlAttributeValueOn(){
+    const res = document.getElementById('demo');
+    res.src ="./assets/pic_bulbon.gif";
+}
+
+function changeHtmlAttributeValueOff(){
+    const res = document.getElementById('demo');
+    res.src ="./assets/pic_bulboff.gif";
+}
