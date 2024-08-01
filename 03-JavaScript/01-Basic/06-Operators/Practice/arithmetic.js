ef248101605fa97a85079c934105c64a443cb3a0
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2024-08-01 14:59:20 +0530

    Update arithmetic.js

--- 03-JavaScript/01-Basic/06-Operators/Practice/arithmetic.js
+++ 03-JavaScript/01-Basic/06-Operators/Practice/arithmetic.js
@@ -1,13 +1,13 @@
 let a = 10, b = 5
 
-let addition, substraction, multiplication, division, modulo, exponentiation, pre_increment, post_increment, pre_decrement, post_decrement
+let addition, subtraction, multiplication, division, modulo, exponentiation, pre_increment, post_increment, pre_decrement, post_decrement
 
 
 addition = a + b
 console.log(addition);  // o.p: 15
 
-substraction = a - b
-console.log(substraction);  // o.p: 5
+subtraction = a - b
+console.log(subtraction);  // o.p: 5
 
 multiplication = a * b
 console.log(multiplication);    // o.p: 50
@@ -21,7 +21,6 @@ console.log(modulo);    // o.p: 0
 
 exponentiation = a ** b
 console.log(exponentiation);    // o.p: 100000
-
 
 post_increment = a++
 console.log(post_increment);  // o.p: 10
